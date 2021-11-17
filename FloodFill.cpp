3a442264f9b3b66a5cc8cb10c9345b373b0c2cef
SubhadeepZilong <63146468+SubhadeepZilong@users.noreply.github.com>
2021-11-17 15:31:20 +0530

    code

---
+++ FloodFill.cpp
@@ -0,0 +1,24 @@
+#include <graphics.h>
+#include <conio.h>
+void flood(int,int,int,int);  
+main()
+{
+    int gd = DETECT, gm;
+
+    initgraph(&gd, &gm, ""); 
+	rectangle(50,50,250,250);  
+    flood(55,55,4,0);  
+    getch();  
+}  
+void flood(int x,int y,int fillColor,int defaultColor)
+{  
+    if(getpixel(x,y)==defaultColor)  
+    {  
+        //delay(1);  
+        putpixel(x,y,fillColor);  
+        flood(x+1,y,fillColor,defaultColor);  
+        flood(x-1,y,fillColor,defaultColor);  
+        flood(x,y+1,fillColor,defaultColor);  
+        flood(x,y-1,fillColor,defaultColor);  
+    }  
+}  
